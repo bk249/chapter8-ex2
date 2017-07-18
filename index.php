a7b4232177baf0675777252df33adad4ca929bcf
Bee Kim <bk249@njit.edu>
2017-07-18 14:32:03 -0400

    find the total score

--- index.php
+++ index.php
@@ -32,9 +32,10 @@ switch ($action) {
 
         // process the scores
         // TODO: Add code that calculates the score total
-        $scores_string = '';
+        $scores_string = 0;
         foreach ($scores as $s) {
             $scores_string .= $s . '|';
+            $score_total += $s;
         }
         $scores_string = substr($scores_string, 0, strlen($scores_string)-1);
 
